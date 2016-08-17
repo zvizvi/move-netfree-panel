e7cb19bff3de907b8c810ead7507573d8c528466
zvizvi <5633910@gmail.com>
2016-08-17 19:28:33 +0300

    v0.3

--- move-netfree-panel/js/main.js
+++ move-netfree-panel/js/main.js
@@ -1,7 +1,7 @@
 //mnfp
 $(document).ready(function () {
 	mnfp = {
-		version: '0.2.5'
+		version: '0.3'
 	};
 	mnfp.window = document.getElementById('netfree-popup-window');
 	mnfp.main = document.getElementById('netfree-popup-window-main');
