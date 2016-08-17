532e5b887c7d3d9df28bded60d2605a5f135ad43
zvizvi <5633910@gmail.com>
2016-08-17 18:45:23 +0300

    v0.2 Dragging to the left side

--- move-netfree-panel/background.js
+++ move-netfree-panel/background.js
@@ -4,45 +4,74 @@ chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
 			code: "mnfp = {\
 					version: '0.1'\
 				};\
-				mnfp.pop = document.getElementById('netfree-popup-window-main');\
+				mnfp.window = document.getElementById('netfree-popup-window');\
+				mnfp.main = document.getElementById('netfree-popup-window-main');\
 				mnfp.handDrag = document.createElement('span');\
 				mnfp.handDrag.id = 'netfree-popup-window-hand-drag';\
-				mnfp.pop.appendChild(mnfp.handDrag);\
+				mnfp.main.appendChild(mnfp.handDrag);\
 			\
 				mnfp.style = document.createElement('style');\
 				mnfp.style.innerHTML = '#netfree-popup-window-hand-drag{position:absolute;top:-10px;left:0;right:0;height:10px;z-index:5;background-color:#769897;cursor:move;}';\
 				mnfp.style.innerHTML += '#netfree-popup-window-main{user-select:none;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;-o-user-select:none;}';\
+				mnfp.style.innerHTML += '#netfree-popup-window.left #netfree-popup-window-main{left:-170px;right:auto;transition:300ms ease-in-out left;}#netfree-popup-window.left #netfree-popup-window-hand-pull{left:auto;right:-30px;transform:rotateZ(180deg);height:105px;}#netfree-popup-window.left #netfree-popup-window-main.active{left:0;}';\
+				mnfp.style.innerHTML += '#netfree-popup-window.dragging #netfree-popup-window-main{transition:0ms right,0ms left;}';\
+			\
 				mnfp.style.type = 'text/css';\
-				mnfp.pop.appendChild(mnfp.style);\
+				mnfp.main.appendChild(mnfp.style);\
 			\
 				mnfp.frame = document.getElementById('netfree-popup-window-iframe');\
 			\
 				mnfp.drag = false;\
 				mnfp.handDrag.addEventListener('mousedown', function(){\
 					mnfp.drag = true;\
+					mnfp.window.className += ' dragging';\
 					mnfp.frame.style.pointerEvents = 'none';\
 				});\
 				document.addEventListener('mouseup', function(e){\
-					mnfp.drag = false;\
-					mnfp.frame.style.pointerEvents = '';\
-					if(e.target != mnfp.handDrag){\
-						mnfp.pop.className = '';\
-					};\
+					if(mnfp.drag == true){\
+						mnfp.drag = false;\
+						mnfp.main.style.right = '';\
+						mnfp.main.style.left = '';\
+						mnfp.frame.style.pointerEvents = '';\
+						if(e.target != mnfp.handDrag){\
+							mnfp.main.className = '';\
+						};\
+						if(e.pageX < (window.innerWidth/2)){\
+							mnfp.window.className = 'left';\
+						}else{\
+							mnfp.window.className = '';\
+						};\
+					}\
 				});\
 			\
 				mnfp.windowHeight = window.innerHeight-90;\
+				mnfp.windowWidth = window.innerWidth;\
 				window.addEventListener('resize', function(){\
 					mnfp.windowHeight = window.innerHeight-90;\
+					mnfp.windowWidth = window.innerWidth;\
 				});\
 			\
 				document.addEventListener('mousemove', function(e){\
 					if(mnfp.drag === false) return;\
-					var y = e.clientY;console.log(window.innerHeight);\
+					var y = e.clientY;\
+					var x = mnfp.windowWidth-e.clientX;\
 					if(y > 4 && y < mnfp.windowHeight){\
-						mnfp.pop.style.top = (y+5)+'px';\
+						mnfp.main.style.top = (y+5)+'px';\
 					};\
-					mnfp.pop.className = 'active';\
-				});"
+					if(x < 170){\
+						mnfp.main.style.right = '';\
+						mnfp.window.className = 'dragging'\
+					}else if(e.clientX < 170){\
+						mnfp.main.style.right = 'auto';\
+						mnfp.main.style.left = '0px';\
+						mnfp.window.className += ' left'\
+					}else{\
+						mnfp.main.style.right = (x-100)+'px';\
+						mnfp.main.style.left = 'auto';\
+					};\
+					mnfp.main.className = 'active';\
+				});\
+				"
 		});
 	}
 })
